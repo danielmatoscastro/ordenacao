a145f78744a82cd0d863c7207c2e844b03e0fbac
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2018-08-24 16:53:51 -0300

    Adição do shellsort

--- insercao.py
+++ insercao.py
@@ -1,3 +1,4 @@
+from math import log, floor
 from aleatorio import lista_aleatoria
 
 
@@ -15,7 +16,7 @@ def insertionsort(lista):
 
 def busca_binaria(lista, inicio, fim, elemento):
     meio = ((fim - inicio) // 2) + inicio
-    
+
     if lista[meio] == elemento or inicio >= fim:
         return meio
     elif lista[meio] < elemento:
@@ -43,9 +44,31 @@ def insertionsort_busca_binaria(lista):
             lista[posicao] = elemento
 
 
+def insertionsort_shell(lista, h):
+    for k in range(0, h):
+        for i in range(k+h, len(lista), h):
+
+            elemento = lista[i]
+            j = i - h
+
+            while j >= 0 and lista[j] > elemento:
+                lista[j+h] = lista[j]
+                j = j - h
+
+            lista[j+h] = elemento
+
+
+def shell_sort(lista):
+    h = 2**floor(log(len(lista), 2))
+
+    while h >= 1:
+        insertionsort_shell(lista, h)
+        h = h // 2
+
+
 if __name__ == '__main__':
-    lista = lista_aleatoria(10)
+    lista = lista_aleatoria(8)
     print(lista)
 
-    insertionsort_busca_binaria(lista)
+    shell_sort(lista)
     print(lista)
