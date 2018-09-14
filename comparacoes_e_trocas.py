72d85943c47a9f17c116e8e4374058ded3b4e3ea
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2018-09-14 16:35:31 -0300

    Remoção do combsort

--- comparacoes_e_trocas.py
+++ comparacoes_e_trocas.py
@@ -28,8 +28,36 @@ def combsort(lista, fator=1.3):
         gap = floor(gap / fator)
 
 
+def particiona(lista, pi, pf)?
+    pp = pi
+    i = pi + 1
+    j = pf
+
+    while j > i:
+        while c[i] < c[pp] and i < pf:
+            i += 1
+        while c[j] > c[pp] and j > pi:
+            i -= 1
+        if c[i] < c[j]:
+            aux = c[i]
+            c[i] = c[j]
+            c[j] = aux
+    aux = c[j]
+    c[j] = c[pp]
+    c[pp] = aux
+
+    return j
+
+
+def quicksort(lista, inicio, fim):
+    if fim > inicio:
+        pivo = particiona(lista, inicio, fim)
+        quicksort(lista, inicio, pivo-1)
+        quicksort(lista, pivo+1, fim)
+
+
 if __name__ == '__main__':
     lista = lista_aleatoria(10)
     print(lista)
-    combsort(lista)
+    quicksort
     print(lista)
